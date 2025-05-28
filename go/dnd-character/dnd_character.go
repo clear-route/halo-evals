edbd1d361d844792d479499cf8e519efb7f1779f
Halo <halo@clearroute.io>
2025-05-28 00:23:38 +0000

    feat(dnd-character): Implement D&D character generation and pass all tests

--- go/dnd-character/dnd_character.go
+++ go/dnd-character/dnd_character.go
@@ -1,5 +1,13 @@
 package dndcharacter
 
+import (
+	"math"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+// Character represents a D&D character with abilities and hitpoints.
 type Character struct {
 	Strength     int
 	Dexterity    int
@@ -10,17 +18,44 @@ type Character struct {
 	Hitpoints    int
 }
 
-// Modifier calculates the ability modifier for a given ability score
+// Seed the random number generator
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Modifier calculates the ability modifier for a given ability score.
+// The modifier is calculated by subtracting 10 from the score, dividing by 2, and rounding down.
 func Modifier(score int) int {
-	panic("Please implement the Modifier() function")
+	return int(math.Floor(float64(score-10) / 2.0))
 }
 
-// Ability uses randomness to generate the score for an ability
+// rollDie simulates rolling a single d6 die.
+func rollDie() int {
+	return rand.Intn(6) + 1 // Generates a random integer between 0 and 5, then adds 1
+}
+
+// Ability uses randomness to generate the score for an ability.
+// This is done by rolling four 6-sided dice and summing the largest three dice.
 func Ability() int {
-	panic("Please implement the Ability() function")
+	rolls := make([]int, 4)
+	for i := 0; i < 4; i++ {
+		rolls[i] = rollDie()
+	}
+	sort.Ints(rolls) // Sorts in ascending order
+	// Sum the three largest dice (rolls[1], rolls[2], rolls[3] after sorting)
+	return rolls[1] + rolls[2] + rolls[3]
 }
 
-// GenerateCharacter creates a new Character with random scores for abilities
+// GenerateCharacter creates a new Character with random scores for abilities and calculated hitpoints.
 func GenerateCharacter() Character {
-	panic("Please implement the GenerateCharacter() function")
+	constitution := Ability()
+	return Character{
+		Strength:     Ability(),
+		Dexterity:    Ability(),
+		Constitution: constitution,
+		Intelligence: Ability(),
+		Wisdom:       Ability(),
+		Charisma:     Ability(),
+		Hitpoints:    10 + Modifier(constitution),
+	}
 }
